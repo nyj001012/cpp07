551eb721130ed22b88e4cf7e0e2cb8d510dea9d4
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-09 20:18:28 +0900

    feat(ex02): implement ex02

---
+++ ex02/Array.hpp
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 19:13:07 by yena              #+#    #+#             */
+/*   Updated: 2023/10/09 20:15:55 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//
+// Created by 나예진 on 2023/10/09.
+//
+
+#ifndef EX02__ARRAY_HPP_
+#define EX02__ARRAY_HPP_
+
+#include <iostream>
+
+template <typename T>
+class Array {
+ private:
+  T *_data;
+  unsigned int _size;
+
+ public:
+  Array();
+  Array(unsigned int n);
+  Array(const Array<T> &other);
+  Array<T> &operator=(const Array<T> &other);
+  ~Array();
+  T &operator[](unsigned int idx) const;
+  unsigned int size() const;
+};
+
+template <typename T>
+Array<T>::Array() {
+  _data = new T[0];
+  _size = 0;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n) {
+  _data = new T[n];
+  _size = n;
+}
+
+template <typename T>
+Array<T>::Array(const Array<T> &other) {
+  _data = new T[other._size];
+  _size = other._size;
+  for (unsigned i = 0; i < _size; i++) {
+	_data[i] = other._data[i];
+  }
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array<T> &other) {
+  if (this != &other) {
+	delete[] _data;
+	_data = new T[other._size];
+	_size = other._size;
+	for (int i = 0; i < _size; i++) {
+	  _data[i] = other._data[i];
+	}
+  }
+  return *this;
+}
+
+template <typename T>
+Array<T>::~Array() {
+  delete[] _data;
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int idx) const {
+  if (idx >= _size) {
+	throw std::exception();
+  }
+  return _data[idx];
+}
+
+template <typename T>
+unsigned int Array<T>::size() const {
+  return _size;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const Array<T> &arr) {
+  os << "[";
+  for (unsigned int i = 0; i < arr.size(); i++) {
+	os << arr[i];
+	if (i != arr.size() - 1) {
+	  os << ", ";
+	}
+  }
+  os << "]";
+  return os;
+}
+
+#endif //EX02__ARRAY_HPP_
---
+++ ex02/main.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 19:12:51 by yena              #+#    #+#             */
+/*   Updated: 2023/10/09 20:17:50 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+int main() {
+  Array<int> emptyArr;
+  Array<int> iArr(5);
+  Array<std::string> sArr(3);
+
+  std::cout << "-------------- EMPTY ARRAY (INT) --------------" << std::endl;
+  std::cout << "size: " << (&emptyArr)->size() << std::endl;
+  std::cout << "data: " << emptyArr << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "----------------- ARRAY (INT) -----------------" << std::endl;
+  std::cout << "size: " << (&iArr)->size() << std::endl;
+  std::cout << "data: " << iArr << std::endl;
+  std::cout << "[INFO]: Change data[0] to 100" << std::endl;
+  iArr[0] = 100;
+  std::cout << "data: " << iArr << std::endl;
+  std::cout << "[INFO]: Make exception" << std::endl;
+  try {
+	sArr[4] = "exception";
+  } catch (std::exception &e) {
+	std::cout << e.what() << std::endl;
+  }
+  std::cout << std::endl;
+
+  std::cout << "---------------- ARRAY (STRING) ---------------" << std::endl;
+  std::cout << "size: " << (&sArr)->size() << std::endl;
+  std::cout << "data: " << sArr << std::endl;
+  std::cout << "[INFO]: Change data[2] to \"yena\"" << std::endl;
+  sArr[2] = "yena";
+  std::cout << "data: " << sArr << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "----------------- COPY ARRAY ------------------" << std::endl;
+  Array<int> copyArr(iArr);
+  std::cout << "origin:\tsize: " << (&copyArr)->size() << std::endl;
+  std::cout << "origin:\tdata: " << copyArr << std::endl;
+  std::cout << "copy:\tsize: " << (&iArr)->size() << std::endl;
+  std::cout << "copy:\tdata: " << iArr << std::endl;
+
+  std::cout << "-------------- ASSIGNMENT ARRAY ---------------" << std::endl;
+  Array<std::string> assignArr;
+  assignArr = sArr;
+  std::cout << "origin:\tsize: " << (&sArr)->size() << std::endl;
+  std::cout << "origin:\tdata: " << sArr << std::endl;
+  std::cout << "assign:\tsize: " << (&assignArr)->size() << std::endl;
+  std::cout << "assign:\tdata: " << assignArr << std::endl;
+  return 0;
+}
