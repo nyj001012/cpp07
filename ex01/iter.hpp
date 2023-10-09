936bddd332a2908d69d6372b296d5f7326d1ca6b
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-09 19:11:20 +0900

    feat(ex01): implement ex01

---
+++ ex01/iter.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 18:52:15 by yena              #+#    #+#             */
+/*   Updated: 2023/10/09 19:03:08 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//
+// Created by 나예진 on 2023/10/09.
+//
+
+#ifndef EX01__ITER_HPP_
+#define EX01__ITER_HPP_
+
+#include <iostream>
+
+template <typename T>
+void print(T const &x) {
+  std::cout << x << std::endl;
+}
+
+template <typename T>
+void iter(T *arr, int len, void (*func)(T const &)) {
+  for (int i = 0; i < len; i++) {
+	func(arr[i]);
+  }
+}
+
+#endif //EX01__ITER_HPP_
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,35 @@
-#include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 18:49:31 by yena              #+#    #+#             */
+/*   Updated: 2023/10/09 19:09:44 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::srand(std::time(NULL));
+  int iArr[5] = { 42, 24, 10, 12, 3 };
+  std::string strArr[5] = {
+	  "\033[32mP\033[0mizza",
+	  "\033[32mA\033[0mpple",
+	  "\033[32mS\033[0mtrawberry",
+	  "\033[32mS\033[0mnowman"
+  };
+  double dArr[4] = { 3.14, 1.414, 2.718, 1.618 };
+
+  std::cout << "-------------------------- ITER INT --------------------------" << std::endl;
+  ::iter(iArr, 5, print);
+  std::cout << std::endl;
+  std::cout << "------------------------- ITER STRING -------------------------" << std::endl;
+  ::iter(strArr, 5, print);
+  std::cout << "------------------------- ITER DOUBLE -------------------------" << std::endl;
+  ::iter(dArr, 4, print);
+
   return 0;
 }
