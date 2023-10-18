f9da804d4806e934be0e98e7bafd1bed58da8edf
yena <nyj001012@gmail.com>
2023-10-18 13:36:04 +0900

    style: :lipstick: add font colour and reformat output

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,32 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: yena <yena@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 18:49:31 by yena              #+#    #+#             */
-/*   Updated: 2023/10/09 19:09:44 by yena             ###   ########.fr       */
+/*   Updated: 2023/10/18 13:35:25 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "font.hpp"
 #include "iter.hpp"
 
 int main() {
   std::srand(std::time(NULL));
-  int iArr[5] = { 42, 24, 10, 12, 3 };
-  std::string strArr[5] = {
-	  "\033[32mP\033[0mizza",
-	  "\033[32mA\033[0mpple",
-	  "\033[32mS\033[0mtrawberry",
-	  "\033[32mS\033[0mnowman"
-  };
-  double dArr[4] = { 3.14, 1.414, 2.718, 1.618 };
+  int iArr[5] = {42, 24, 10, 12, 3};
+  std::string strArr[5] = {"\033[33mP\033[0mizza", "\033[33mA\033[0mpple",
+                           "\033[33mS\033[0mtrawberry",
+                           "\033[33mS\033[0mnowman"};
+  double dArr[4] = {3.14, 1.414, 2.718, 1.618};
 
-  std::cout << "-------------------------- ITER INT --------------------------" << std::endl;
+  std::cout << F_GREEN << "------------- ITER INT -------------" << FB_DEFAULT
+            << std::endl;
   ::iter(iArr, 5, print);
   std::cout << std::endl;
-  std::cout << "------------------------- ITER STRING -------------------------" << std::endl;
+
+  std::cout << F_GREEN << "------------ ITER STRING -----------" << FB_DEFAULT
+            << std::endl;
   ::iter(strArr, 5, print);
-  std::cout << "------------------------- ITER DOUBLE -------------------------" << std::endl;
+  std::cout << std::endl;
+
+  std::cout << F_GREEN << "------------ ITER DOUBLE -----------" << FB_DEFAULT
+            << std::endl;
   ::iter(dArr, 4, print);
 
   return 0;
