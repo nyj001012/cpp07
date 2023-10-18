7dbf971d110a9f9b2ec777738a14a31a3c27ea2f
yena <nyj001012@gmail.com>
2023-10-18 13:40:30 +0900

    style: :lipstick: add font colour

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,23 +6,26 @@
 /*   By: yena <yena@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 19:12:51 by yena              #+#    #+#             */
-/*   Updated: 2023/10/18 13:25:24 by yena             ###   ########.fr       */
+/*   Updated: 2023/10/18 13:39:36 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#include "font.hpp"
 
 int main() {
   Array<int> emptyArr;
   Array<int> iArr(5);
   Array<std::string> sArr(3);
 
-  std::cout << "-------------- EMPTY ARRAY (INT) --------------" << std::endl;
+  std::cout << F_GREEN << "-------------- EMPTY ARRAY (INT) --------------"
+            << FB_DEFAULT << std::endl;
   std::cout << "size: " << (&emptyArr)->size() << std::endl;
   std::cout << "data: " << emptyArr << std::endl;
   std::cout << std::endl;
 
-  std::cout << "----------------- ARRAY (INT) -----------------" << std::endl;
+  std::cout << F_GREEN << "----------------- ARRAY (INT) -----------------"
+            << FB_DEFAULT << std::endl;
   std::cout << "size: " << (&iArr)->size() << std::endl;
   std::cout << "data: " << iArr << std::endl;
   std::cout << "[INFO]: Change data[0] to 100" << std::endl;
@@ -36,7 +39,8 @@ int main() {
   }
   std::cout << std::endl;
 
-  std::cout << "---------------- ARRAY (STRING) ---------------" << std::endl;
+  std::cout << F_GREEN << "---------------- ARRAY (STRING) ---------------"
+            << FB_DEFAULT << std::endl;
   std::cout << "size: " << (&sArr)->size() << std::endl;
   std::cout << "data: " << sArr << std::endl;
   std::cout << "[INFO]: Change data[2] to \"yena\"" << std::endl;
@@ -44,7 +48,8 @@ int main() {
   std::cout << "data: " << sArr << std::endl;
   std::cout << std::endl;
 
-  std::cout << "----------------- COPY ARRAY ------------------" << std::endl;
+  std::cout << F_GREEN << "----------------- COPY ARRAY ------------------"
+            << FB_DEFAULT << std::endl;
   Array<int> copyArr(iArr);
   std::cout << "origin:\tsize: " << (&copyArr)->size() << std::endl;
   std::cout << "origin:\tdata: " << copyArr << std::endl;
@@ -52,12 +57,14 @@ int main() {
   std::cout << "copy:\tdata: " << iArr << std::endl;
   std::cout << std::endl;
 
-  std::cout << "-------------- ASSIGNMENT ARRAY ---------------" << std::endl;
+  std::cout << F_GREEN << "-------------- ASSIGNMENT ARRAY ---------------"
+            << FB_DEFAULT << std::endl;
   Array<std::string> assignArr;
   assignArr = sArr;
   std::cout << "origin:\tsize: " << (&sArr)->size() << std::endl;
   std::cout << "origin:\tdata: " << sArr << std::endl;
   std::cout << "assign:\tsize: " << (&assignArr)->size() << std::endl;
   std::cout << "assign:\tdata: " << assignArr << std::endl;
+
   return 0;
 }
